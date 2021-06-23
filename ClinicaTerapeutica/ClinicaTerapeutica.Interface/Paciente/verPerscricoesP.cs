cc80cfbaf886edabbb6377444f275defaf261212
ricardoalexp <61251343+ricardoalexp@users.noreply.github.com>
2021-06-23 18:38:57 +0100

    Update verPerscricoesP.cs

--- ClinicaTerapeutica/ClinicaTerapeutica.Interface/Paciente/verPerscricoesP.cs
+++ ClinicaTerapeutica/ClinicaTerapeutica.Interface/Paciente/verPerscricoesP.cs
@@ -1,7 +1,6 @@
 ﻿using ClinicaTerapeutica.Data.Entidades.Modelos;
 using ClinicaTerapeutica.Funcionalidade.Gestores.GestorMarcacoes;
-using ClinicaTerapeutica.Data.Entidades.Modelos;
-using ClinicaTerapeutica.Funcionalidade.Gestores.GestorMarcacoes;using System;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
