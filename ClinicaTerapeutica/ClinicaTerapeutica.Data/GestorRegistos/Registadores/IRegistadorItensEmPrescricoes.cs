c66579158e6d71f555daa63de14764966c61b3c7
ricardoalexp <ricardoalexp17@gmail.com>
2021-06-24 18:38:30 +0100

    asda

---
+++ ClinicaTerapeutica/ClinicaTerapeutica.Data/GestorRegistos/Registadores/IRegistadorItensEmPrescricoes.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClinicaTerapeutica.Data.GestorRegistos.Registadores
+{
+    interface IRegistadorItensEmPrescricoes
+    {
+        bool RegistarMedicamentoEmPrescricao(int idPrescricao, int idMedicamento);
+        bool RegistarTratamentoEmPrescricao(int idPrescricao, int idTratamento);
+        bool RegistarExerciciosEmPrescricao(int idPrescricao, int idExercicio);
+    }
+}
